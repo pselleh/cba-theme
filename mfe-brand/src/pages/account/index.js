5baac34a0e05cd04eb9763539be338240d74937d
Phil Selleh <pselleh@gmail.com>
2026-05-08 00:26:32 +0000

    Update production theme

---
+++ mfe-brand/src/pages/account/index.js
@@ -0,0 +1,6 @@
+export const pageConfig = {
+  name: "REPLACE_PAGE_NAME",
+  layout: "default",
+};
+
+export default pageConfig;
---
+++ mfe-brand/src/theme/colors.js
@@ -0,0 +1,4 @@
+export const colors = {
+  primary: '#0A2A66',
+  secondary: '#F5A623',
+};
